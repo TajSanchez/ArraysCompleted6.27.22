dd21c7166e5dbc4ce965e89a0e2782ae5979b1b0
Taj <tajmarie92@gmail.com>
2022-06-27 21:57:33 -0500

    Add project files.

---
+++ Arrays6.27.22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays6._27._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateGrid();
+
+            Console.ReadKey();
+
+        }
+
+        public static void CreateGrid()
+        { 
+            int width = 20;
+            int height = 20;
+
+            int[,] grid = new int[width, height];
+            
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Console.Write(grid[x, y] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
